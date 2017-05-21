c4b49213eec940c5a48f1b5e715a7d045fef86ed
Wil_D <wil.donovan@gmail.com>
2017-05-20 21:30:21 -0500

    checking and reassigning letter to ord value

--- lib/player_ship_placement.rb
+++ lib/player_ship_placement.rb
@@ -7,14 +7,22 @@ class PlayerShipPlacement
 	end
 
 	def pt_boat_placement
-		print "Please choose 2 adjacent coordinates to place your PT boat" + "\n" 
+		print "Please choose 2 adjacent coordinates to place your PT boat" + "\n"
 			answer = gets.chomp
 			binding.pry
 			check(answer)
 	end
 
 	def check(answer)
+		new_answer = answer.join.scan(/.{1}/)
+		new_answer.each.with_index do |input, index|
+			new_answer[index] = input.chr if input != (1..10)
+		end
+		insert_into_library
+	end
 
+	def insert_into_library
+		
 	end
 
 
